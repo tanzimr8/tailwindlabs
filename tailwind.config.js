97c6fc837428e074ea73651413898eaa5d4a05e5
Tanzim <tanzim008@gmail.com>
2024-07-09 23:23:13 -0230

    customized tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px',
+    },
+    extend: {
+      colors:{
+        brandColor: '#003135',
+        textColor: '#333',
+      }
+    },
   },
   plugins: [],
 }
